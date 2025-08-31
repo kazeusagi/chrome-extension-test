0b7a5d704bba183251986cd3bde7a996f52e6eb8
kazeusagi <toshiki1098@gmail.com>
2025-08-31 12:30:01 +0000

    feat: iconとmanifestの修正

--- extensions/aws-account-name-popup/src/entrypoints/popup/App.tsx
+++ extensions/aws-account-name-popup/src/entrypoints/popup/App.tsx
@@ -1,11 +1,8 @@
-import { useState } from "react";
-import reactLogo from "@/assets/react.svg";
-import wxtLogo from "/wxt.svg";
-import "./App.css";
 import type { Account } from "@/types";
 
+import "./App.css";
+
 async function App() {
-  const [count, setCount] = useState(0);
   const accounts: Account[] | null = await storage.getItem("local:accounts");
 
   if (!accounts) return <div>No accounts found.</div>;
--- extensions/aws-account-name-popup/wxt.config.ts
+++ extensions/aws-account-name-popup/wxt.config.ts
@@ -3,8 +3,9 @@ import { defineConfig } from "wxt";
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   srcDir: "src",
-  modules: ["@wxt-dev/module-react"],
+  modules: ["@wxt-dev/module-react", "@wxt-dev/auto-icons"],
   manifest: {
+    name: "AWS Account Name",
     permissions: ["activeTab", "storage"],
   },
 });
